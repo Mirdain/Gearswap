c41327b2b626fa3e0dab40e8734ce92f34f14414
Mirdain <34732910+Mirdain@users.noreply.github.com>
2020-04-21 14:33:03 -0500

    Add files via upload

---
+++ RDM.lua
@@ -0,0 +1,324 @@
+
+--Inyana
+
+-- Load and initialize the include file.
+include('Mirdain-Include')
+
+--Set to ingame lockstyle and Macro Book/Set
+LockStylePallet = "6"
+MacroBook = "3"
+MacroSet = "1"
+
+function get_sets()
+
+	-- Standard Idle set with -DT,Refresh,Regen and movement gear
+	sets.Idle = {
+		main="Daybreak",
+		sub="Genmei Shield",
+		ammo="Staunch Tathlum +1",
+		head={ name="Viti. Chapeau +3", augments={'Enfeebling Magic duration','Magic Accuracy',}},
+		body={ name="Amalric Doublet +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		hands="Malignance Gloves",
+		legs={ name="Lengo Pants", augments={'INT+7','Mag. Acc.+7','"Mag.Atk.Bns."+3','"Refresh"+1',}},
+		feet={ name="Chironic Slippers", augments={'CHR+4','Attack+21','"Refresh"+2','Mag. Acc.+19 "Mag.Atk.Bns."+19',}},
+		neck="Loricate Torque +1",
+		waist="Fucho-no-Obi",
+		left_ear="Hearty Earring",
+		right_ear="Etiolation Earring",
+		left_ring="Defending Ring",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+    }
+	--Set used for pure -DT when not engaged (no TP considerations and Augments the Idle set)
+	sets.DT = {
+	}
+	sets.Movement = {
+		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
+	}
+	sets.Precast = {}
+	-- Used for Magic Spells
+	sets.Precast.FastCast = {
+		main="Daybreak",
+		sub="Sors Shield",
+		ammo="Impatiens",
+		head={ name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4',}},  --14
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}}, --15
+		hands="Gende. Gages +1", --7
+		legs={ name="Kaykaus Tights +1", augments={'MP+80','"Cure" spellcasting time -7%','Enmity-6',}}, --7
+		feet={ name="Carmine Greaves +1", augments={'Accuracy+12','DEX+12','MND+20',}}, --8
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Witful Belt", --2
+		left_ear="Enchntr. Earring +1", --2
+		right_ear="Etiolation Earring", --1
+		left_ring="Veneficium Ring",
+		right_ring="Lebeche Ring",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}}, --10
+	}
+	-- Job Abilities
+	sets.JA = {}
+	sets.JA["Chainspell"] = {body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}}}
+	sets.JA["Saboteur"] = {}
+	sets.JA["Spontaneity"] = {}
+	sets.JA["Stymie"] = {}
+	sets.JA["Convert"] = {}
+	sets.JA["Composure"] = {}
+
+	-- ===================================================================================================================
+	--		sets.midcast
+	-- ===================================================================================================================
+
+	-- Default song duration / strength
+	sets.Midcast = {}
+	--This set is used as base as is overwrote by specific gear changes (Spell Interruption Rate Down)
+	sets.Midcast.SIRD = {
+		hands={ name="Chironic Gloves", augments={'Mag. Acc.+28','Spell interruption rate down -9%','INT+12',}}, --19
+		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}}, --20
+		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}}, --16
+	    right_ring="Freke Ring", --10
+		waist="Rumination Sash", --10
+		ammo="Staunch Tathlum +1", --11
+		right_ear="Magnetic Earring", --8
+		--104 with Merits
+	}
+	-- Cure Set
+	sets.Midcast.Cure = {
+		main="Daybreak",
+		sub="Sors Shield",
+		ammo="Hydrocera",
+		head={ name="Kaykaus Mitra +1", augments={'MP+80','"Cure" spellcasting time -7%','Enmity-6',}},
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}},
+		hands={ name="Kaykaus Cuffs +1", augments={'MP+80','MND+12','Mag. Acc.+20',}},
+		legs={ name="Kaykaus Tights +1", augments={'MP+80','"Cure" spellcasting time -7%','Enmity-6',}},
+		feet={ name="Kaykaus Boots +1", augments={'MP+80','"Cure" spellcasting time -7%','Enmity-6',}},
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Luminary Sash",
+		left_ear="Beatific Earring",
+		right_ear="Regal Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Janniston Ring",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+    }
+	-- Enhancing Duration on SELF
+	sets.Midcast.Enhancing = {
+		main={ name="Crocea Mors", augments={'Path: C',}},
+		sub="Ammurapi Shield",
+		ammo="Pemphredo Tathlum",
+		head={ name="Telchine Cap", augments={'Enh. Mag. eff. dur. +10',}},
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}},
+		hands="Atrophy Gloves +3",
+		legs="Leth. Fuseau +1",
+		feet="Leth. Houseaux +1",
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Embla Sash",
+		left_ear="Andoaa Earring",
+		right_ear="Augment. Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+	-- Enhancing Duration on OTHERS
+	sets.Midcast.Enhancing.Others = {
+	    main={ name="Crocea Mors", augments={'Path: C',}},
+		sub="Ammurapi Shield",
+		ammo="Pemphredo Tathlum",
+		head="Leth. Chappel +1",
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}},
+		hands="Atrophy Gloves +3",
+		legs="Leth. Fuseau +1",
+		feet="Leth. Houseaux +1",
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Embla Sash",
+		left_ear="Andoaa Earring",
+		right_ear="Augment. Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+	-- Spells that require SKILL
+	sets.Midcast.Enhancing.Skill = {
+	    main={ name="Crocea Mors", augments={'Path: C',}},
+		sub="Ammurapi Shield",
+		ammo="Staunch Tathlum +1",
+		head="Befouled Crown",
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}},
+		hands={ name="Viti. Gloves +3", augments={'Enhancing Magic duration',}},
+		legs="Atrophy Tights +2",
+		feet="Leth. Houseaux +1",
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Olympus Sash",
+		left_ear="Andoaa Earring",
+		right_ear="Augment. Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+	-- Spells that require elemental (Barspells)
+	sets.Midcast.Enhancing.Elemental = {
+
+	}
+	sets.Midcast.Enfeebling = {
+		main={ name="Grioavolr", augments={'Mag. Acc.+27','Mag. Acc.+23 "Mag.Atk.Bns."+23','Pet: "Regen"+1',}},
+		sub="Enki Strap",
+		ammo="Regal Gem",
+		head={ name="Viti. Chapeau +3", augments={'Enfeebling Magic duration','Magic Accuracy',}},
+		body={ name="Viti. Tabard +3", augments={'Enhances "Chainspell" effect',}},
+		hands={ name="Kaykaus Cuffs +1", augments={'MP+80','MND+12','Mag. Acc.+20',}},
+		legs={ name="Chironic Hose", augments={'Mag. Acc.+25','"Mag.Atk.Bns."+25','Accuracy+5 Attack+5','Mag. Acc.+17 "Mag.Atk.Bns."+17',}},
+		feet={ name="Vitiation Boots +3", augments={'Immunobreak Chance',}},
+		neck={ name="Dls. Torque +2", augments={'Path: A',}},
+		waist="Luminary Sash",
+		left_ear="Digni. Earring",
+		right_ear="Regal Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+
+	sets.Midcast.Enfeebling.MACC = set_combine(sets.Midcast.Enfeebling, {
+
+	})
+
+	sets.Midcast.Enfeebling.Potency = set_combine(sets.Midcast.Enfeebling, {
+
+	})
+
+	-- Specific gear for spells
+	sets.Midcast["Stoneskin"] = set_combine(sets.Midcast.Enhancing, {
+		ring1="Stikini Ring +1",
+		ring2="Stikini Ring +1",
+		waist="Siegel Sash",
+		neck="Nodens Gorget",
+	})
+	sets.Midcast["Aquaveil"] = set_combine(sets.Midcast.Enhancing, {
+		head="Amalric Coif +1"
+	})
+	sets.Midcast.Refresh = set_combine(sets.Midcast.Enhancing, {
+		head="Amalric Coif +1",
+		body="Atrophy Tabard +3",
+		legs="Leth. Fuseau +1",
+	})
+	sets.Midcast.Nuke = {
+		ammo="Pemphredo Tathlum",
+		head={ name="Amalric Coif +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		body="Jhakri Robe +2",
+		hands={ name="Amalric Gages +1", augments={'INT+12','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		legs={ name="Amalric Slops +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		neck="Mizu. Kubikazari",
+		waist="Eschan Stone",
+		left_ear="Friomisi Earring",
+		right_ear="Malignance Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+
+	-- ===================================================================================================================
+	--		sets.aftercast
+	-- ===================================================================================================================
+	--Custome sets for each jobsetup
+	sets.Custom = {}
+	sets.Custom.Saboteur = {
+	    hands="Leth. Gantherots +1",
+	}
+
+	sets.TP = {
+	    main={ name="Crocea Mors", augments={'Path: C',}},
+		sub="Genmei Shield",
+		ammo="Ginsen",
+		head="Malignance Chapeau",
+		body="Malignance Tabard",
+		hands="Malignance Gloves",
+		legs="Malignance Tights",
+		feet={ name="Chironic Slippers", augments={'CHR+4','Attack+21','"Refresh"+2','Mag. Acc.+19 "Mag.Atk.Bns."+19',}},
+		neck="Anu Torque",
+		waist="Witful Belt",
+		left_ear="Sherida Earring",
+		right_ear="Digni. Earring",
+		left_ring="Stikini Ring +1",
+		right_ring="Stikini Ring +1",
+		back={ name="Sucellos's Cape", augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Phys. dmg. taken-10%',}},
+	}
+	sets.TP.DW = {}
+	sets.TP.DT = {}
+	sets.TP.ACC = {}
+
+	sets.WS = {}
+	--This set is used when OffenseMode is ACC and a WS is used (Augments the WS base set)
+	sets.WS.ACC = {}
+	-- Note that the Mote library will unlock these gear spots when used.
+	sets.TreasureHunter = {
+		waist="Chaac Belt",
+	    head="Wh. Rarab Cap +1",
+	}
+
+	organizer_items  = {		
+		item1 = "Echo Drops",
+		item2 = "Remedy",
+		item3 = "Holy Water",
+	}
+	--Command to Lock Style and Set the correct macros
+	jobsetup (LockStylePallet,MacroBook,MacroSet)
+end
+
+-------------------------------------------------------------------------------------------------------------------
+-- DO NOT EDIT BELOW THIS LINE UNLESS YOU NEED TO MAKE JOB SPECIFIC RULES
+-------------------------------------------------------------------------------------------------------------------
+
+--Adjust custom precast actions
+function pretarget_custom(spell,action)
+
+end
+-- Augment basic equipment sets
+function precast_custom(spell)
+	equipSet = {}
+
+	return equipSet
+end
+-- Augment basic equipment sets
+function midcast_custom(spell)
+	equipSet = {}
+	if buffactive['Saboteur'] then
+		equipSet = sets.Custom.Saboteur
+	end
+	return equipSet
+end
+-- Augment basic equipment sets
+function aftercast_custom(spell)
+	equipSet = {}
+	if not buffactive['Composure'] and spell.name ~=  "Composure" then
+		add_to_chat(8,'Composure is down')
+	end
+	return equipSet
+end
+--Function is called when the player gains or loses a buff
+function buff_change_custom(name,gain)
+	equipSet = {}
+
+	return equipSet
+end
+--This function is called when a update request the correct equipment set
+function choose_set_custom()
+	equipSet = {}
+
+	return equipSet
+end
+--Function is called when the player changes states
+function status_change_custom(new,old)
+	equipSet = {}
+	if T{'Idle','Resting'}:contains(new) then
+		if player.tp > 500 then
+			windower.add_to_chat(8,'TP over 500 Locking weapons')
+			disable('main')
+			disable('sub')
+		else
+			enable('main')
+			enable('sub')
+		end
+	end
+	return equipSet
+end
+--Function is called when a self command is issued
+function self_command_custom(command)
+
+end
