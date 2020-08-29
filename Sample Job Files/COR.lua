fa16f3746b4ed969cdc1cbf0941ce757fb9ac524
Mirdain <34732910+Mirdain@users.noreply.github.com>
2020-08-29 02:37:59 -0500

    Add files via upload

--- Sample Job Files/COR.lua
+++ Sample Job Files/COR.lua
@@ -4,27 +4,25 @@
 include('Mirdain-Include')
 
 --Set to ingame lockstyle and Macro Book/Set
-LockStylePallet = "9"
-MacroBook = "9"
+LockStylePallet = "18"
+MacroBook = "18"
 MacroSet = "1"
 
--- TP Ammo
-ammo.bullet.RA = "Decimating Bullet"
--- Physical Weaponskills
-ammo.bullet.WS = "Eminent Bullet"
--- Magical Weaponskills
-ammo.bullet.MAB = "Adlivun Bullet"
--- Magic Accuracy
-ammo.bullet.MACC = "Adlivun Bullet"
--- Quick Draw
-ammo.bullet.QD = "Adlivun Bullet"
+-- load addons
+send_command('lua l autocor')
+
+-- Initialize Player
+jobsetup (LockStylePallet,MacroBook,MacroSet)
 
 -- Threshold for Ammunition Warning
-ammo_warning_limit = 50
+Ammo_Warning_Limit = 50
 
 function get_sets()
+
 	-- Standard Idle set with -DT,Refresh,Regen with NO movement gear
 	sets.Idle = {
+	    main={ name="Rostam", augments={'Path: C',}},
+		sub={ name="Rostam", augments={'Path: A',}},
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
@@ -38,21 +36,36 @@ function get_sets()
 		right_ring="Shadow Ring",
 		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
     }
+
 	sets.Movement = {
 		legs="Carmine Cuisses +1",
 	}
+
+	-- TP Ammo
+	Ammo.Bullet.RA = "Decimating Bullet"
+	-- Physical Weaponskills
+	Ammo.Bullet.WS = "Eminent Bullet"
+	-- Magical Weaponskills
+	Ammo.Bullet.MAB = "Adlivun Bullet"
+	-- Magic Accuracy
+	Ammo.Bullet.MACC = "Adlivun Bullet"
+	-- Quick Draw
+	Ammo.Bullet.QD = "Adlivun Bullet"
 
 	sets.Precast = {}
 	-- 70 snapshot is Cap.  Need 60 due to 10 from gifts
 	-- Snapshot / Rapidshot
 	-- Rapid shot is like quick magic
 	-- Snapshot is like Fast Cast
+	-- Flurry is 15%
+	-- Flurry II 30% Snapshot
 
-	--No flurry - max Snapshot gear
+	--No flurry - 60 Snapshot needed
 	sets.Precast.RA = {
-		ammo=ammo.bullet.RA,
-	    head="Chass. Tricorne +1", -- 0/14
-		--Taeon Chapeau
+		main={ name="Rostam", augments={'Path: A',}},
+		sub="Nusku Shield",
+		ammo=Ammo.Bullet.RA,
+		head={ name="Taeon Chapeau", augments={'"Snapshot"+5','"Snapshot"+5',}}, -- 10/0
 		body="Oshosi Vest +1", -- 14/0
 		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
 		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
@@ -61,83 +74,72 @@ function get_sets()
 		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
 		-- Totals 62/24
     }
-	-- Flurry
+
+	-- Flurry - 45 Snapshot Needed
 	sets.Precast.RA.Flurry = {
-		ammo=ammo.bullet.RA,
-	    head="Chass. Tricorne +1", -- 0/14
-		-- Need Taeon Chapeau for +10 Snapshot
-		body="Oshosi Vest +1", -- 14/0
-		-- Need COR AF body for Snapshot +20
-		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
-		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
-		feet="Meg. Jam. +2", -- 10/0
-		waist="Yemaya Belt", -- 0/10
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
-    } 
-	-- Flurry II
-	sets.Precast.RA.Flurry_II = {
-		ammo=ammo.bullet.RA,
-	    head="Chass. Tricorne +1", -- 0/14
-		body="Oshosi Vest +1", -- 14/0
-		-- Need COR AF body for Rapid shot +20
-		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
-		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
-		feet="Meg. Jam. +2", -- 10/0
-		-- Persuers Feet for Rapidshot
-		waist="Yemaya Belt", -- 0/10
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
-    } 
-	sets.Precast.Enmity = {}
-	sets.Midcast = {}
-	-- Ranged Attack Gear (Normal Midshot)
-    sets.Midcast.RA = {
-		ammo=ammo.bullet.RA,
 		main={ name="Rostam", augments={'Path: A',}},
 		sub="Nusku Shield",
-        head="Malignance Chapeau",
-        body="Malignance Tabard",
-        hands="Malignance Gloves",
-        legs="Malignance Tights",
-        feet="Malignance Boots",
-        neck="Iskur Gorget",
-        ear1="Enervating Earring",
-		ear2="Neritic Earring",
-        --ear2="Telos Earring",
-        ring1="Dingir Ring",
-        ring2="Ilabrat Ring",
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
-        waist="Yemaya Belt",
+		ammo=Ammo.Bullet.RA,
+	    head="Chass. Tricorne +1", -- 0/14
+		head={ name="Taeon Chapeau", augments={'"Snapshot"+5','"Snapshot"+5',}}, -- 10/0
+		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
+		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
+		feet="Meg. Jam. +2", -- 10/0
+		waist="Yemaya Belt", -- 0/10
+		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
+    } -- Totals 48/48
+
+	-- Flurry II - 30 Snapshot Needed
+	sets.Precast.RA.Flurry_II = {
+		main={ name="Rostam", augments={'Path: A',}},
+		sub="Nusku Shield",
+		ammo=Ammo.Bullet.RA,
+	    head="Chass. Tricorne +1", -- 0/14
+		body="Laksa. Frac +3", -- 0/20
+		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
+		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
+		feet={ name="Pursuer's Gaiters", augments={'Rng.Acc.+10','"Rapid Shot"+10','"Recycle"+15',}}, -- 0/10
+		waist="Yemaya Belt", -- 0/10
+		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
+    } -- Totals 28/78
+
+	sets.Midcast = {}
+
+	-- Ranged Attack Gear (Normal Midshot)
+    sets.Midcast.RA = {
+		head="Malignance Chapeau",
+		body="Malignance Tabard",
+		hands="Malignance Gloves",
+		legs="Malignance Tights",
+		feet="Malignance Boots",
+		neck="Iskur Gorget",
+		waist="Yemaya Belt",
+		left_ear="Telos Earring",
+		right_ear="Enervating Earring",
+		left_ring="Dingir Ring",
+		right_ring="Ilabrat Ring",
+		back={ name="Camulus's Mantle", augments={'AGI+20','Rng.Acc.+20 Rng.Atk.+20','Rng.Acc.+10','"Store TP"+10','Phys. dmg. taken-10%',}},
     }
+
 	-- Ranged Attack Gear (Triple Shot Midshot)
-	sets.Midcast.RA.TripleShot = {
-		ammo=ammo.bullet.RA,
+	sets.Midcast.RA.TripleShot = set_combine(sets.Midcast.RA, {
         head="Oshosi Mask +1", --5
         body="Chasseur's Frac +1", --12
         hands="Lanun Gants +3",
         legs="Osh. Trousers +1", --6
         feet="Osh. Leggings +1", --3
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
-    } --27
+    }) --27
 
-	--This set is used as base as is overwrote by specific gear changes (Spell Interruption Rate Down)
-	sets.Midcast.SIRD = {}
-	-- Cure Set
-	sets.Midcast.Cure = {}
-	-- Enhancing Skill
-	sets.Midcast.Enhancing = {}
-	-- High MACC for landing spells
-	sets.Midcast.Enfeebling = {}
-	-- Specific gear for spells
 	sets.Midcast["Stoneskin"] = {
 		waist="Siegel Sash",
 	}
+
 	-- Job Abilities
 	sets.JA = {}
 	sets.JA["Wild Card"] = {
 	    feet={ name="Lanun Bottes +3", augments={'Enhances "Wild Card" effect',}},
 	}
 	sets.JA["Phantom Roll"] = {}
-	sets.JA["Double-UP"] = {}
 	sets.JA["Quick Draw"] = {
 	    feet="Chass. Bottes +1",
 	}
@@ -155,22 +157,15 @@ function get_sets()
 	sets.JA["Crooked Cards"] = {}
 
 	--Base Set used for all rolls
-	sets.PhantomRoll = {
+	sets.PhantomRoll = set_combine(sets.Idle, {
 		main={ name="Rostam", augments={'Path: C',}}, -- +8 Effect and 60 sec Duration
 		range="Compensator", -- 20 sec Duration
 		head={ name="Lanun Tricorne +3", augments={'Enhances "Winning Streak" effect',}}, -- 50% Job ability Bonus
-		body="Malignance Tabard",
 		hands="Chasseur's Gants +1", --50 sec Duration
-		legs="Malignance Tights",
-		feet="Malignance Boots",
 		neck="Regal Necklace", -- 20 sec Duration
-		waist="Flume Belt +1",
-		left_ear="Sanare Earring",
-		right_ear="Thureous Earring",
-		left_ring="Defending Ring",
 		right_ring="Luzaf's Ring", -- 16 yalm range
 		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 30 sec Duration
-	}
+	})
 	sets.PhantomRoll['Fighter\'s Roll'] = sets.PhantomRoll
 	sets.PhantomRoll['Healers\'s Roll'] = sets.PhantomRoll
 	sets.PhantomRoll['Warlocks\'s Roll'] = sets.PhantomRoll
@@ -198,8 +193,6 @@ function get_sets()
 
 	--Base TP set to build off when melee'n
 	sets.TP = {
-		range="Compensator",
-		ammo="Eminent Bullet",
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
@@ -213,54 +206,87 @@ function get_sets()
 		right_ring="Ilabrat Ring",
 		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
 	}
+
 	--This set is used when OffenseMode is DT and Enaged (Augments the TP base set)
 	sets.TP.DT = {
+		main={ name="Rostam", augments={'Path: C',}},
+		sub={ name="Rostam", augments={'Path: A',}},
 		range="Compensator",
+		ammo="Eminent Bullet",
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
 		legs="Malignance Tights",
 		feet="Malignance Boots",
 		neck="Iskur Gorget",
-		waist="Sarissapho. Belt",
-		left_ear="Suppanomimi",
-		right_ear="Brutal Earring",
-		left_ring="Rajas Ring",
-		right_ring="Petrov Ring",
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
+		waist="Reiki Yotai",
+		left_ear="Telos Earring",
+		right_ear="Suppanomimi",
+		left_ring="Petrov Ring",
+		right_ring="Ilabrat Ring",
+		back={ name="Camulus's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
 	}
+
 	--The following sets augment the base TP set
 	sets.TP.DW = {
+		main="Naegling",
+		sub="Blurred Knife +1",
 		range="Compensator",
+		ammo="Eminent Bullet",
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
 		legs="Malignance Tights",
 		feet="Malignance Boots",
-		neck="Iskur Gorget",
+		neck="Loricate Torque +1",
+		waist="Reiki Yotai",
+		left_ear="Telos Earring",
+		right_ear="Suppanomimi",
+		left_ring="Petrov Ring",
+		right_ring="Ilabrat Ring",
+		back={ name="Camulus's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
+	}
+
+	--The following sets augment the base TP set
+	sets.TP.RA = {
+		head="Malignance Chapeau",
+		body="Malignance Tabard",
+		hands="Malignance Gloves",
+		legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
+		feet="Malignance Boots",
+		neck="Loricate Torque +1",
 		waist="Sarissapho. Belt",
-		left_ear="Suppanomimi",
-		right_ear="Brutal Earring",
-		left_ring="Rajas Ring",
-		right_ring="Petrov Ring",
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
+		left_ear="Telos Earring",
+		right_ear="Suppanomimi",
+		left_ring="Petrov Ring",
+		right_ring="Ilabrat Ring",
+		back={ name="Camulus's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dual Wield"+10','Phys. dmg. taken-10%',}},
 	}
+
+	--The following sets augment the base TP set
+	sets.TP.DW.DT = {}
+
+	--The following sets augment the base TP set
+	sets.TP.RA.DT = {}
+
 	--This set is used when OffenseMode is ACC and Enaged (Augments the TP base set)
-	sets.TP.ACC = {
-	}
+	sets.TP.ACC = {}
+
 	sets.WS = {
-		ammo=ammo.bullet.WS,
+		ammo=Ammo.Bullet.WS,
 	}
+
 	--This set is used when OffenseMode is ACC and a WS is used (Augments the WS base set)
-	sets.WS.ACC = {
-	}
-	sets.WS.WSD = {
-	}
+	sets.WS.ACC = {}
+
+	sets.WS.WSD = {}
+
 	sets.WS.MAB = {
-		ammo=ammo.bullet.MAB,
+		ammo=Ammo.Bullet.MAB,
 	}
+
 	sets.WS.MACC = {
-		ammo=ammo.bullet.MACC,
+		ammo=Ammo.Bullet.MACC,
 	}
 
 	sets.WS["Hot Shot"] = {}
@@ -278,18 +304,49 @@ function get_sets()
 	sets.WS["Shining Blade"] = {}
 	sets.WS["Circle Blade"] = {}
 	sets.WS["Spirits Within"] = {}
-	sets.WS["Savage Blade"] = {}
+	sets.WS["Savage Blade"] = {
+		head={ name="Herculean Helm", augments={'Accuracy+20 Attack+20','Weapon skill damage +4%',}},
+		body="Laksa. Frac +3",
+		hands="Meg. Gloves +2",
+		legs={ name="Herculean Trousers", augments={'Attack+21','Weapon skill damage +5%','Accuracy+15',}},
+		feet={ name="Lanun Bottes +3", augments={'Enhances "Wild Card" effect',}},
+		neck="Comm. Charm +2",
+		waist="Grunfeld Rope",
+		left_ear={ name="Moonshade Earring", augments={'Attack+4','TP Bonus +250',}},
+		right_ear="Ishvara Earring",
+		left_ring="Karieyh Ring",
+		right_ring="Regal Ring",
+		back={ name="Camulus's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%',}},
+	}
 	sets.WS["Requiescat"] = {}
 
 	sets.QuickDraw = {
-		ammo=ammo.bullet.MAB
+		ammo=Ammo.Bullet.MAB
 	}
 	sets.QuickDraw.ACC = {}
 	sets.QuickDraw.DMG = {}
 	sets.QuickDraw.STP = {}
 
-	--Custome sets for each jobsetup
+	--Custom sets for each jobsetup
 	sets.Custom = {}
+
+	sets.Charm = {
+	    main="Lament",
+		range="Compensator",
+		ammo="Decimating Bullet",
+		head="Malignance Chapeau",
+		body="Malignance Tabard",
+		hands="Malignance Gloves",
+		legs="Malignance Tights",
+		feet="Malignance Boots",
+		neck="Loricate Torque +1",
+		waist="Yemaya Belt",
+		left_ear="Sanare Earring",
+		right_ear="Thureous Earring",
+		left_ring="Defending Ring",
+		right_ring="Shadow Ring",
+		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
+	}
 
 	sets.TreasureHunter = {
 
@@ -300,7 +357,6 @@ function get_sets()
 		item2 = "Remedy",
 		item3 = "Holy Water",
 	}	
-	jobsetup (LockStylePallet,MacroBook,MacroSet)
 end
 
 -------------------------------------------------------------------------------------------------------------------
@@ -347,7 +403,12 @@ function status_change_custom(new,old)
 
 	return equipSet
 end
+
 --Function is called when a self command is issued
 function self_command_custom(command)
 
+end
+
+function user_file_unload()
+	send_command('lua u autocor')
 end
