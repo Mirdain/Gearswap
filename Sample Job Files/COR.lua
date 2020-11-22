8543efaef5a4273a0ded159c614496eb6e2e5549
Mirdain <34732910+Mirdain@users.noreply.github.com>
2020-11-21 22:35:48 -0600

    Add files via upload

--- Sample Job Files/COR.lua
+++ Sample Job Files/COR.lua
@@ -20,7 +20,7 @@ UI_Name = 'DPS'
 --Modes for specific to Corsair
 state.JobMode = M{['description']='Corsair Damage Mode'}
 state.JobMode:options('Naegling','Fomalhaut','Death Penalty') -- Can add Crit for Armageddon
-state.JobMode:set('Naegling')
+state.JobMode:set('Death Penalty')
 
 -- load addons
 send_command('lua l autocor')
@@ -52,7 +52,7 @@ function get_sets()
 
 	sets.Weapons['Death Penalty'] = {
 		main={ name="Rostam", augments={'Path: A'}, bag="Wardrobe 4", priority=2},
-		range="Death Penalty",
+		range={ name="Death Penalty", augments={'Path: A',}},
 		sub={ name="Rostam", augments={'Path: C'}, bag="Wardrobe 2", priority=1},
 	}
 
@@ -63,7 +63,7 @@ function get_sets()
 	-- Ammo Selection
 	Ammo.Bullet.RA = "Chrono Bullet"	-- TP Ammo
 	Ammo.Bullet.WS = "Chrono Bullet"	-- Physical Weaponskills
-	Ammo.Bullet.MAB = "Chrono Bullet"	-- Magical Weaponskills
+	Ammo.Bullet.MAB = "Living Bullet"	-- Magical Weaponskills
 	Ammo.Bullet.MACC = "Chrono Bullet"	-- Magic Accuracy
 	Ammo.Bullet.QD = "Chrono Bullet"	-- Quick Draw
 
@@ -75,11 +75,11 @@ function get_sets()
 		hands="Malignance Gloves",
 		legs="Malignance Tights",
 		feet="Malignance Boots",
-		neck="Warder's Charm",
-		waist="Flume Belt +1",
-		left_ear="Tuisto Earring",
-		right_ear="Sanare Earring",
-		left_ring="Gelatinous Ring +1",
+		neck={ name="Comm. Charm +2", augments={'Path: A',}}, -- 4/0
+		waist="Liv. Bul. Pouch",
+		left_ear="Sanare Earring",
+		right_ear={ name="Odnowa Earring +1", augments={'Path: A',}},
+		left_ring={ name="Gelatinous Ring +1", augments={'Path: A',}},
 		right_ring="Shadow Ring",
 		back={ name="Camulus's Mantle", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
     }
@@ -92,10 +92,8 @@ function get_sets()
 
 	--Base TP set to build off when melee'n
 	sets.OffenseMode.TP = {
-		main="Naegling",
-		sub="Blurred Knife +1",
+		ammo = Ammo.Bullet.RA,
 		range="Fomalhaut",
-		ammo="Chrono Bullet",
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
@@ -112,8 +110,8 @@ function get_sets()
 
 	--This set is used when OffenseMode is DT and Enaged
 	sets.OffenseMode.DT = set_combine(sets.OffenseMode.TP, {
-		neck="Loricate Torque +1",
-		left_ring="Defending Ring",
+	    sub={ name="Rostam", augments={'Path: C',}},
+		left_ring={ name="Gelatinous Ring +1", augments={'Path: A',}},
 	})
 
 	--The following sets augment the base TP set above for Dual Wielding
@@ -138,7 +136,7 @@ function get_sets()
 		ammo=Ammo.Bullet.RA,
 		head={ name="Taeon Chapeau", augments={'"Snapshot"+5','"Snapshot"+5',}}, -- 10/0
 		body="Oshosi Vest +1", -- 14/0
-		hands={ name="Carmine Fin. Ga. +1", augments={'Rng.Atk.+20','"Mag.Atk.Bns."+12','"Store TP"+6',}}, -- 8/11
+		hands={ name="Carmine Fin. Ga. +1", augments={'HP+80','STR+12','INT+12',}}, -- 8/11
 		legs={ name="Adhemar Kecks +1", augments={'AGI+12','"Rapid Shot"+13','Enmity-6',}}, -- 10/13
 		feet="Meg. Jam. +2", -- 10/0
 		left_ear={ name="Tuisto Earring", priority=2},
@@ -147,7 +145,7 @@ function get_sets()
 		right_ring={ name="Regal Ring", priority=4},
 		neck={ name="Comm. Charm +2", augments={'Path: A',}}, -- 4/0
 		waist="Yemaya Belt", -- 0/10
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
+		back={ name="Camulus's Mantle", augments={'HP+60','HP+20','"Snapshot"+10',}}, -- 10/0
     } -- Totals 66/24
 
 	sets.Precast.RA.ACC = {}
@@ -166,7 +164,7 @@ function get_sets()
 		right_ring={ name="Regal Ring", priority=4},
 		neck={ name="Comm. Charm +2", augments={'Path: A',}}, -- 4/0
 		waist="Yemaya Belt", -- 0/10
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
+		back={ name="Camulus's Mantle", augments={'HP+60','HP+20','"Snapshot"+10',}}, -- 10/0
     } -- Totals 52/54
 
 	-- Flurry II - 30 Snapshot Needed
@@ -183,7 +181,7 @@ function get_sets()
 		right_ring={ name="Regal Ring", priority=4},
 		neck={ name="Comm. Charm +2", augments={'Path: A',}}, -- 4/0
 		waist="Yemaya Belt", -- 0/10
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 10/0
+		back={ name="Camulus's Mantle", augments={'HP+60','HP+20','"Snapshot"+10',}}, -- 10/0
     } -- Totals 32/78
 
 	-- Fast Cast for Magic
@@ -193,6 +191,7 @@ function get_sets()
 
 	-- Ranged Attack Gear (Normal Midshot)
     sets.Midcast.RA = {
+		ammo=Ammo.Bullet.RA,
 		head="Malignance Chapeau",
 		body="Malignance Tabard",
 		hands="Malignance Gloves",
@@ -277,8 +276,8 @@ function get_sets()
 	sets.JA["Snake Eye"] = {
 	    legs={ name="Lanun Trews +1", augments={'Enhances "Snake Eye" effect',}},
 	}
-	sets.JA["Fold"] = {} -- Use gloves for double bust (below in custom function)
-	sets.JA["Triple Shot"] = {} --Gear to be worn during Midshot
+	sets.JA["Fold"] = {}			-- Use gloves for bust
+	sets.JA["Triple Shot"] = {}		-- Gear to be worn during Midshot
 	sets.JA["Cutting Cards"] = {}
 	sets.JA["Crooked Cards"] = {}
 
@@ -291,8 +290,9 @@ function get_sets()
 		hands="Chasseur's Gants +1", --50 sec Duration
 		neck="Regal Necklace", -- 20 sec Duration
 		right_ring="Luzaf's Ring", -- 16 yalm range
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}}, -- 30 sec Duration
+		back={ name="Camulus's Mantle", augments={'HP+60','HP+20','"Snapshot"+10',}}, -- 30 sec Duration
 	})
+
 	sets.PhantomRoll['Fighter\'s Roll'] = sets.PhantomRoll
 	sets.PhantomRoll['Monk\'s Roll'] = sets.PhantomRoll
 	sets.PhantomRoll['Healer\'s Roll'] = sets.PhantomRoll
@@ -359,7 +359,6 @@ function get_sets()
 	sets.WS["Slug Shot"] = {}
 	sets.WS["Numbing Shot"] = {}
 	sets.WS["Last Stand"] = {
-		ammo="Chrono Bullet",
 		head={ name="Lanun Tricorne +3", augments={'Enhances "Winning Streak" effect',}},
 		body="Laksa. Frac +3",
 		hands="Meg. Gloves +2",
@@ -373,16 +372,18 @@ function get_sets()
 		right_ring="Regal Ring",
 		back={ name="Camulus's Mantle", augments={'STR+20','Accuracy+20 Attack+20','STR+10','Weapon skill damage +10%',}},
 	}
+
 	sets.WS["Wildfire"] = {}
 	sets.WS["Leaden Salute"] = {
-		ammo="Chrono Bullet",
+		ammo=Ammo.Bullet.MAB,
 		head="Pixie Hairpin +1",
 		body={ name="Lanun Frac +3", augments={'Enhances "Loaded Deck" effect',}},
 		hands={ name="Herculean Gloves", augments={'"Mag.Atk.Bns."+18','Weapon skill damage +5%','STR+2','Mag. Acc.+15',}},
 		legs={ name="Herculean Trousers", augments={'Mag. Acc.+14 "Mag.Atk.Bns."+14','Weapon skill damage +4%','Mag. Acc.+4','"Mag.Atk.Bns."+15',}},
 		feet={ name="Lanun Bottes +3", augments={'Enhances "Wild Card" effect',}},
 		neck={ name="Comm. Charm +2", augments={'Path: A',}},
-		waist="Svelt. Gouriz +1",
+		waist="Orpheus's Sash",
+		--waist="Svelt. Gouriz +1",   -- Will need to make variable distance choice
 		left_ear="Friomisi Earring",
 		right_ear={ name="Moonshade Earring", augments={'Attack+4','TP Bonus +250',}},
 		left_ring="Dingir Ring",
@@ -417,7 +418,7 @@ function get_sets()
 		right_ear="Thureous Earring",
 		left_ring="Defending Ring",
 		right_ring="Shadow Ring",
-		back={ name="Camulus's Mantle", augments={'"Snapshot"+10',}},
+		back={ name="Camulus's Mantle", augments={'HP+60','HP+20','"Snapshot"+10',}},
 	}
 
 	sets.TreasureHunter = {
