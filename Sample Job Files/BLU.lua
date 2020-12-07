17682e88057f12d322a019587d2b19dcdb2588cf
Mirdain <34732910+Mirdain@users.noreply.github.com>
2020-12-06 20:48:57 -0600

    Add files via upload

--- Sample Job Files/BLU.lua
+++ Sample Job Files/BLU.lua
@@ -1,5 +1,5 @@
 
---Mirdain
+--Salidar
 
 -- Load and initialize the include file.
 include('Mirdain-Include')
@@ -9,6 +9,12 @@ LockStylePallet = "11"
 MacroBook = "8"
 MacroSet = "2"
 
+-- Use "gs c food" to use the specified food item 
+Food = "Sublime Sushi"
+
+--Set default mode (TP,ACC,DT)
+state.OffenseMode:set('DT')
+
 --Command to Lock Style and Set the correct macros
 jobsetup (LockStylePallet,MacroBook,MacroSet)
 
@@ -16,52 +22,104 @@ BlueNuke = S{'Spectral Floe','Entomb', 'Magic Hammer', 'Tenebral Crush'}
 BlueHealing = S{'Magic Fruit'}
 BlueSkill = S{'Occultation','Erratic Flutter','Nature\'s Meditation','Cocoon','Barrier Tusk','Matellic Body','Mighty Guard'}
 
---Text for the keybind
-CustomBind = "Cleave Mode"
+--Modes for specific to Corsair
+state.JobMode = M{['description']='Blue Mage Mode'}
+state.JobMode:options('TP','Cleave') -- Can add Crit for Armageddon
+state.JobMode:set('Cleave')
+
+--Enable JobMode for UI
+UI_Name = 'Mode'
+
 --Command to bind to the f9 key
-send_command('bind f9 gs c ToggleCleave')
+send_command('bind f9 gs c togglecleave')
 --Log Message about what the key does
-add_to_chat(8,'[F9] - Cleave Mode is ['..state.CleaveMode.value..']')
+add_to_chat(8,'[F9] - '..UI_Name..' is ['..state.JobMode.value..']')
 
 function get_sets()
+
+	--Set the weapon options.  This is set below in job customization section
+
+	-- Weapon setup
+	sets.Weapons = {}
+
+	sets.Weapons.TP = {
+		main="Almace",
+	}
+
+	sets.Weapons.Cleave = {
+		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
+		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
+	}
+
+	sets.Weapons.Shield = {
+		sub="Genmei Shield",
+	}
+
 	-- Standard Idle set with -DT,Refresh,Regen and movement gear
 	sets.Idle = {
-		main="Earth Staff",
 		ammo="Staunch Tathlum +1",
 		head="Malignance Chapeau",
-		body="Jhakri Robe +2",
+		body="Shamash Robe",
 		hands="Malignance Gloves",
 		legs="Malignance Tights",
 		feet="Malignance Boots",
 		neck="Loricate Torque +1",
 		waist="Flume Belt +1",
 		left_ear="Etiolation Earring",
-		right_ear="Thureous Earring",
-		left_ring="Stikini Ring +1",
+		right_ear="Tuisto Earring",
+		left_ring="Defending Ring",
 		right_ring="Stikini Ring +1",
-		back="Moonbeam Cape",
+		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
     }
 	sets.Movement = {
 		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
     }
+
+	sets.OffenseMode = {}
+
+	sets.OffenseMode.TP = {
+	    main="Almace",
+		sub="Naegling",
+		ammo="Ginsen",
+		head={ name="Adhemar Bonnet +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		hands={ name="Adhemar Wrist. +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
+		feet={ name="Herculean Boots", augments={'Attack+22','"Triple Atk."+3',}},
+		neck="Lissome Necklace",
+		waist="Windbuffet Belt +1",
+		left_ear="Suppanomimi",
+		right_ear="Telos Earring",
+		left_ring="Epona's Ring",
+		right_ring="Petrov Ring",
+		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
+	}
+	sets.OffenseMode.DW = {}
+	sets.OffenseMode.DT = {}
+	sets.OffenseMode.ACC = {}
+
+	sets.DualWield = {
+		main="Almace",
+		sub="Zantetsuken",
+	}
+
+
 	sets.Precast = {}
 	-- Used for Magic Spells
 	sets.Precast.FastCast = {
-	    main={ name="Colada", augments={'Phys. dmg. taken -2%','Accuracy+23','Attack+23','DMG:+20',}},
-		sub="Genmei Shield",
-		ammo="Sapience Orb", --2
-		head={ name="Herculean Helm", augments={'Accuracy+15','"Fast Cast"+5','INT+9','Mag. Acc.+9','"Mag.Atk.Bns."+13',}}, --12
-		body={ name="Taeon Tabard", augments={'"Fast Cast"+5',}}, --9
-		hands={ name="Leyline Gloves", augments={'Accuracy+15','Mag. Acc.+15','"Mag.Atk.Bns."+15','"Fast Cast"+3',}}, --8
-		legs="Aya. Cosciales +2", --6
-		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}}, --6
-		neck="Voltsurge Torque", --4
-		waist="Witful Belt", --3
-		left_ear="Etiolation Earring", --1
-		right_ear="Loquac. Earring", --2
-		left_ring="Kishar Ring", --4
-		right_ring="Prolix Ring", --3
-		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Damage taken-5%',}},
+		ammo="Sapience Orb",
+		head={ name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4',}},
+		body="Hashishin Mintan +1",
+		hands="Hashi. Bazu. +1",
+		legs="Aya. Cosciales +2",
+		feet={ name="Carmine Greaves +1", augments={'Accuracy+12','DEX+12','MND+20',}},
+		neck="Voltsurge Torque",
+		waist="Emphatikos Rope",
+		left_ear="Enchntr. Earring +1",
+		right_ear="Loquac. Earring",
+		left_ring="Kishar Ring",
+		right_ring="Rahab Ring",
+		back={ name="Rosmerta's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Spell interruption rate down-10%',}},
 	}
 	-- Job Abilities
 	sets.JA = {}
@@ -73,11 +131,6 @@ function get_sets()
 	sets.JA["Unbridled Learning"] = {}
 	sets.JA["Unbridled Wisdom"] = {}
 
-	-- ===================================================================================================================
-	--		sets.midcast
-	-- ===================================================================================================================
-
-	-- Default song duration / strength
 	sets.Midcast = {}
 	--This set is used as base as is overwrote by specific gear changes (Spell Interruption Rate Down)
 	sets.Midcast.SIRD = { --Total = 15 merits + 84 gear = 99 - Cap is 105
@@ -93,16 +146,17 @@ function get_sets()
 	-- Cure Set
 	sets.Midcast.Cure = {
 		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		ammo="Hydrocera",
-		head="Aya. Zucchetto +2",
+		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
+		ammo="Sapience Orb",
+		head={ name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4',}},
 		body="Jhakri Robe +2",
-		hands="Aya. Manopolas +2",
+		hands="Aya. Manopolas +1",
 		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
-		feet={ name="Medium's Sabots", augments={'MP+45','MND+9','"Conserve MP"+5','"Cure" potency +4%',}},
+		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
 		neck="Phalaina Locket",
-		waist="Gishdubar Sash",
+		waist="Tempus Fugit",
 		left_ear="Mendi. Earring",
-		right_ear="Etiolation Earring",
+		right_ear="Regal Earring",
 		left_ring="Stikini Ring +1",
 		right_ring="Stikini Ring +1",
 		back="Solemnity Cape",
@@ -132,49 +186,36 @@ function get_sets()
 	sets.Midcast.Nuke = {
 		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
 		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		ammo="Hydrocera",
-		head={ name="Amalric Coif +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		ammo="Pemphredo Tathlum",
+		head="Jhakri Coronal +1",
 		body="Jhakri Robe +2",
 		hands={ name="Amalric Gages +1", augments={'INT+12','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
 		legs={ name="Amalric Slops +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
 		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
-		neck="Sanctity Necklace",
+		neck="Eddy Necklace",
 		waist="Eschan Stone",
-		left_ear="Friomisi Earring",
-		right_ear="Hermetic Earring",
-		left_ring="Jhakri Ring",
+		left_ear="Enchntr. Earring +1",
+		right_ear="Regal Earring",
+		left_ring="Stikini Ring +1",
 		right_ring="Stikini Ring +1",
-		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Damage taken-5%',}},
+		back="Solemnity Cape",
 	}
 
-	-- ===================================================================================================================
-	--		sets.aftercast
-	-- ===================================================================================================================
-	--Custome sets for each jobsetup
-	sets.Custom = {}
-	sets.Custom.Cleave_Idle = {
-		main="Earth Staff",
-		left_ring="Defending Ring", --10/10
-		right_ring={ name="Dark Ring", augments={'Magic dmg. taken -4%','Phys. dmg. taken -4%','Spell interruption rate down -4%',}}, --4/4
+	sets.WS = {
+		ammo="Ginsen",
+		head={ name="Adhemar Bonnet +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		hands={ name="Adhemar Wrist. +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
+		legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
+		feet={ name="Herculean Boots", augments={'Attack+22','"Triple Atk."+3',}},
+		neck="Fotia Gorget",
+		waist="Fotia Belt",
+		left_ear="Brutal Earring",
+		right_ear="Odr Earring",
+		left_ring="Epona's Ring",
+		right_ring="Ilabrat Ring",
+		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
 	}
-	sets.Custom.Weapons = {}
-	sets.Custom.Weapons.Cleave = {
-		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-	}
-	sets.Custom.Weapons.TP = {
-		main="Naegling",
-		sub={ name="Colada", augments={'Phys. dmg. taken -2%','Accuracy+23','Attack+23','DMG:+20',}},
-	}
-
-	sets.TP = {
-
-	}
-	sets.TP.DW = {}
-	sets.TP.DT = {}
-	sets.TP.ACC = {}
-
-	sets.WS = {}
 	--This set is used when OffenseMode is ACC and a WS is used (Augments the WS base set)
 	sets.WS.ACC = {}
 	-- Note that the Mote library will unlock these gear spots when used.
@@ -195,6 +236,11 @@ end
 -- DO NOT EDIT BELOW THIS LINE UNLESS YOU NEED TO MAKE JOB SPECIFIC RULES
 -------------------------------------------------------------------------------------------------------------------
 
+-- Called when the player's subjob changes.
+function sub_job_change_custom(new, old)
+	-- Typically used for Macro pallet changing
+end
+
 --Adjust custom precast actions
 function pretarget_custom(spell,action)
 
@@ -208,64 +254,75 @@ end
 -- Augment basic equipment sets
 function midcast_custom(spell)
 	equipSet = {}
-	if ToggleCleave == "ON" and spell.type == "Magic" then
-		equipSet = sets.Custom.Weapons.Cleave
-	end
-	return equipSet
+
+	return Weapon_Check(equipSet)
 end
 -- Augment basic equipment sets
 function aftercast_custom(spell)
 	equipSet = {}
-	if ToggleCleave == "ON" and player.status == "Idle" then
-		equipSet = set_combine(sets.Idle, sets.Custom.Cleave_Idle)
-	end
-	return equipSet
+
+	return Weapon_Check(equipSet)
 end
 --Function is called when the player gains or loses a buff
 function buff_change_custom(name,gain)
 	equipSet = {}
 
-	return equipSet
+	return Weapon_Check(equipSet)
 end
 --This function is called when a update request the correct equipment set
 function choose_set_custom()
 	equipSet = {}
 
-	return equipSet
+	return Weapon_Check(equipSet)
 end
 --Function is called when the player changes states
 function status_change_custom(new,old)
 	equipSet = {}
-	if T{'Idle','Resting'}:contains(new) then
-		if player.tp > 100 then
-			windower.add_to_chat(8,'TP over 100 Locking weapons')
-			disable('main')
-			disable('sub')
-		else
-			enable('main')
-			enable('sub')
-		end
-	end
-	return equipSet
+
+	return Weapon_Check(equipSet)
 end
 --Function is called when a self command is issued
 function self_command_custom(command)
-	if command == "togglecleave" then
-		if state.CleaveMode.value == "OFF" then
-			state.CleaveMode.value = "ON"
-			send_command('input //aset removeall;wait 1;input //aset spellset magic;input /macro book 8;wait .1; input /macro set 2')
-			choose_set()  
-			equip(sets.Custom.Weapons.Cleave)
-		else
-			send_command('input //aset removeall;wait 1;input //aset spellset tp;input /macro book 8;wait .1; input /macro set 1')
-			state.CleaveMode.value = "OFF"
-			equip(sets.Idle)
-			equip(sets.Custom.Weapons.TP)
+	if command == 'togglecleave' then
+		weaponcheck()
+		for i,v in ipairs(state.JobMode) do
+			if state.JobMode.value == v then
+				if state.JobMode.value ~= state.JobMode[#state.JobMode] then
+					state.JobMode:set(state.JobMode[i+1])
+					send_command('input //aset spellset magic;input /macro book 8;wait .1; input /macro set 2')
+				else
+					state.JobMode:set(state.JobMode[1])
+					send_command('input //aset spellset tp;input /macro book 8;wait .1; input /macro set 1')
+				end
+				info('Mode: ['..state.JobMode.value..']')
+				equip(set_combine(choose_set(),choose_set_custom()))
+				return
+			end
 		end
-		windower.add_to_chat(8,'Cleave mode ['..state.CleaveMode.value..']')
 	end
 end
+
+ function Weapon_Check(equipSet)
+	equipSet = set_combine(equipSet,sets.Weapons[state.JobMode.value])
+	if DualWield == false then
+		equipSet = set_combine(equipSet,sets.Weapons.Shield)
+	end
+	return equipSet
+ end
+
 -- Function is called when the job lua is unloaded
 function user_file_unload()
 
+end
+
+function check_buff_JA()
+	buff = ''
+	--local ja_recasts = windower.ffxi.get_ability_recasts()
+	return buff
+end
+
+function check_buff_SP()
+	buff = ''
+	--local sp_recasts = windower.ffxi.get_spell_recasts()
+	return buff
 end
