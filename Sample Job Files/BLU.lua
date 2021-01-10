149d20dbc4a85b4aef5e4e864d1a4cc51cd562b9
Mirdain <34732910+Mirdain@users.noreply.github.com>
2021-01-10 03:01:47 -0600

    Add files via upload

--- Sample Job Files/BLU.lua
+++ Sample Job Files/BLU.lua
@@ -1,5 +1,5 @@
 
---Salidar
+--Mirdain
 
 -- Load and initialize the include file.
 include('Mirdain-Include')
@@ -9,11 +9,11 @@ LockStylePallet = "11"
 MacroBook = "8"
 MacroSet = "2"
 
+--Set Mode to Damage Taken as Default
+state.OffenseMode:set('DT')
+
 -- Use "gs c food" to use the specified food item 
 Food = "Sublime Sushi"
-
---Set default mode (TP,ACC,DT)
-state.OffenseMode:set('DT')
 
 --Command to Lock Style and Set the correct macros
 jobsetup (LockStylePallet,MacroBook,MacroSet)
@@ -21,10 +21,11 @@ jobsetup (LockStylePallet,MacroBook,MacroSet)
 BlueNuke = S{'Spectral Floe','Entomb', 'Magic Hammer', 'Tenebral Crush'}
 BlueHealing = S{'Magic Fruit'}
 BlueSkill = S{'Occultation','Erratic Flutter','Nature\'s Meditation','Cocoon','Barrier Tusk','Matellic Body','Mighty Guard'}
+BlueTank = S{}
 
---Modes for specific to Corsair
+--Modes for specific to Blue Mage
 state.JobMode = M{['description']='Blue Mage Mode'}
-state.JobMode:options('TP','Cleave') -- Can add Crit for Armageddon
+state.JobMode:options('TP','Cleave')
 state.JobMode:set('Cleave')
 
 --Enable JobMode for UI
@@ -37,13 +38,11 @@ add_to_chat(8,'[F9] - '..UI_Name..' is ['..state.JobMode.value..']')
 
 function get_sets()
 
-	--Set the weapon options.  This is set below in job customization section
-
 	-- Weapon setup
 	sets.Weapons = {}
 
 	sets.Weapons.TP = {
-		main="Almace",
+		main="Naegling",
 	}
 
 	sets.Weapons.Cleave = {
@@ -59,67 +58,48 @@ function get_sets()
 	sets.Idle = {
 		ammo="Staunch Tathlum +1",
 		head="Malignance Chapeau",
-		body="Shamash Robe",
+		body="Malignance Tabard",
 		hands="Malignance Gloves",
 		legs="Malignance Tights",
 		feet="Malignance Boots",
 		neck="Loricate Torque +1",
 		waist="Flume Belt +1",
 		left_ear="Etiolation Earring",
-		right_ear="Tuisto Earring",
-		left_ring="Defending Ring",
+		right_ear="Thureous Earring",
+		left_ring="Stikini Ring +1",
 		right_ring="Stikini Ring +1",
-		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
+		back="Moonbeam Cape",
     }
 	sets.Movement = {
-		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
+		legs={ name="Carmine Cuisses +1", augments={'HP+80','STR+12','INT+12',}},
     }
 
 	sets.OffenseMode = {}
-
-	sets.OffenseMode.TP = {
-	    main="Almace",
-		sub="Naegling",
-		ammo="Ginsen",
-		head={ name="Adhemar Bonnet +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		hands={ name="Adhemar Wrist. +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
-		feet={ name="Herculean Boots", augments={'Attack+22','"Triple Atk."+3',}},
-		neck="Lissome Necklace",
-		waist="Windbuffet Belt +1",
-		left_ear="Suppanomimi",
-		right_ear="Telos Earring",
-		left_ring="Epona's Ring",
-		right_ring="Petrov Ring",
-		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
-	}
-	sets.OffenseMode.DW = {}
+	sets.OffenseMode.TP = {}
 	sets.OffenseMode.DT = {}
 	sets.OffenseMode.ACC = {}
 
 	sets.DualWield = {
-		main="Almace",
-		sub="Zantetsuken",
+		main="Naegling",
+		sub={ name="Colada", augments={'Phys. dmg. taken -2%','Accuracy+23','Attack+23','DMG:+20',}},
 	}
-
 
 	sets.Precast = {}
 	-- Used for Magic Spells
 	sets.Precast.FastCast = {
 		ammo="Sapience Orb",
-		head={ name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4',}},
-		body="Hashishin Mintan +1",
-		hands="Hashi. Bazu. +1",
+		head={ name="Herculean Helm", augments={'Accuracy+15','"Fast Cast"+5','INT+9','Mag. Acc.+9','"Mag.Atk.Bns."+13',}},
+		body={ name="Taeon Tabard", augments={'"Fast Cast"+5','HP+47',}},
+		hands={ name="Leyline Gloves", augments={'Accuracy+15','Mag. Acc.+15','"Mag.Atk.Bns."+15','"Fast Cast"+3',}},
 		legs="Aya. Cosciales +2",
-		feet={ name="Carmine Greaves +1", augments={'Accuracy+12','DEX+12','MND+20',}},
+		feet={ name="Carmine Greaves +1", augments={'HP+80','MP+80','Phys. dmg. taken -4',}},
 		neck="Voltsurge Torque",
-		waist="Emphatikos Rope",
-		left_ear="Enchntr. Earring +1",
+		waist="Witful Belt",
+		left_ear="Etiolation Earring",
 		right_ear="Loquac. Earring",
 		left_ring="Kishar Ring",
-		right_ring="Rahab Ring",
-		back={ name="Rosmerta's Cape", augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','Mag. Acc.+10','"Fast Cast"+10','Spell interruption rate down-10%',}},
+		right_ring="Prolix Ring",
+		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Damage taken-5%',}},
 	}
 	-- Job Abilities
 	sets.JA = {}
@@ -131,6 +111,11 @@ function get_sets()
 	sets.JA["Unbridled Learning"] = {}
 	sets.JA["Unbridled Wisdom"] = {}
 
+	-- ===================================================================================================================
+	--		sets.midcast
+	-- ===================================================================================================================
+
+	-- Default song duration / strength
 	sets.Midcast = {}
 	--This set is used as base as is overwrote by specific gear changes (Spell Interruption Rate Down)
 	sets.Midcast.SIRD = { --Total = 15 merits + 84 gear = 99 - Cap is 105
@@ -145,18 +130,16 @@ function get_sets()
 	}
 	-- Cure Set
 	sets.Midcast.Cure = {
-		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		ammo="Sapience Orb",
-		head={ name="Carmine Mask +1", augments={'Accuracy+20','Mag. Acc.+12','"Fast Cast"+4',}},
-		body="Jhakri Robe +2",
-		hands="Aya. Manopolas +1",
-		legs={ name="Carmine Cuisses +1", augments={'Accuracy+20','Attack+12','"Dual Wield"+6',}},
-		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
-		neck="Phalaina Locket",
-		waist="Tempus Fugit",
+		ammo="Hydrocera",
+		head={ name="Herculean Helm", augments={'Accuracy+15','"Fast Cast"+5','INT+9','Mag. Acc.+9','"Mag.Atk.Bns."+13',}},
+		body="Malignance Tabard",
+		hands={ name="Leyline Gloves", augments={'Accuracy+15','Mag. Acc.+15','"Mag.Atk.Bns."+15','"Fast Cast"+3',}},
+		legs={ name="Lengo Pants", augments={'INT+5','Mag. Acc.+4','"Mag.Atk.Bns."+1','"Refresh"+1',}},
+		feet={ name="Medium's Sabots", augments={'MP+50','MND+10','"Conserve MP"+7','"Cure" potency +5%',}},
+		neck="Incanter's Torque",
+		waist="Gishdubar Sash",
 		left_ear="Mendi. Earring",
-		right_ear="Regal Earring",
+		right_ear="Etiolation Earring",
 		left_ring="Stikini Ring +1",
 		right_ring="Stikini Ring +1",
 		back="Solemnity Cape",
@@ -184,38 +167,26 @@ function get_sets()
 		head="Amalric Coif +1"
 	})
 	sets.Midcast.Nuke = {
-		main={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		sub={ name="Nibiru Cudgel", augments={'MP+50','INT+10','"Mag.Atk.Bns."+15',}},
-		ammo="Pemphredo Tathlum",
-		head="Jhakri Coronal +1",
-		body="Jhakri Robe +2",
+		ammo="Hydrocera",
+		head={ name="Amalric Coif +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
+		body="Malignance Tabard",
 		hands={ name="Amalric Gages +1", augments={'INT+12','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
 		legs={ name="Amalric Slops +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
 		feet={ name="Amalric Nails +1", augments={'MP+80','Mag. Acc.+20','"Mag.Atk.Bns."+20',}},
-		neck="Eddy Necklace",
+		neck="Sanctity Necklace",
 		waist="Eschan Stone",
-		left_ear="Enchntr. Earring +1",
-		right_ear="Regal Earring",
+		left_ear="Regal Earring",
+		right_ear="Hermetic Earring",
 		left_ring="Stikini Ring +1",
 		right_ring="Stikini Ring +1",
-		back="Solemnity Cape",
+		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','Accuracy+10','"Dbl.Atk."+10','Damage taken-5%',}},
 	}
 
-	sets.WS = {
-		ammo="Ginsen",
-		head={ name="Adhemar Bonnet +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		body={ name="Adhemar Jacket +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		hands={ name="Adhemar Wrist. +1", augments={'DEX+12','AGI+12','Accuracy+20',}},
-		legs={ name="Samnuha Tights", augments={'STR+10','DEX+10','"Dbl.Atk."+3','"Triple Atk."+3',}},
-		feet={ name="Herculean Boots", augments={'Attack+22','"Triple Atk."+3',}},
-		neck="Fotia Gorget",
-		waist="Fotia Belt",
-		left_ear="Brutal Earring",
-		right_ear="Odr Earring",
-		left_ring="Epona's Ring",
-		right_ring="Ilabrat Ring",
-		back={ name="Rosmerta's Cape", augments={'DEX+20','Accuracy+20 Attack+20','DEX+10','"Dbl.Atk."+10','Phys. dmg. taken-10%',}},
-	}
+	-- ===================================================================================================================
+	--		sets.aftercast
+	-- ===================================================================================================================
+
+	sets.WS = {}
 	--This set is used when OffenseMode is ACC and a WS is used (Augments the WS base set)
 	sets.WS.ACC = {}
 	-- Note that the Mote library will unlock these gear spots when used.
